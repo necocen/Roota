39c3b616ac8e644b43e0e4366601eb47429a41b0
necocen <necocen@gmail.com>
2021-09-19 10:56:36 +0900

    remove test target from Package.swift

--- Package.swift
+++ Package.swift
@@ -11,11 +11,8 @@ let package = Package(name: "Roota",
                       ],
                       dependencies: [
                           .package(url: "https://github.com/mxcl/PromiseKit", from: "6.0.0"),
-                          .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-                          .package(url: "https://github.com/Quick/Quick", from: "3.0.0")
                       ],
                       targets: [
                           .target(name: "Roota", dependencies: ["PromiseKit"], exclude: ["Info.plist", "Roota.h"]),
-                          .target(name: "RootaUI", dependencies: ["Roota", "PromiseKit"], exclude: ["Info.plist", "RootaUI.h"]),
-                          .testTarget(name: "RootaTests", dependencies: ["Roota", "RootaUI", "Nimble", "Quick"], exclude: ["Info.plist"])
+                          .target(name: "RootaUI", dependencies: ["Roota", "PromiseKit"], exclude: ["Info.plist", "RootaUI.h"])
                       ])
